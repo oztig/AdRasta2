20eafd3a6a52d78c982e51447d44a37f9690293c
oztig <nj_pearson+github@icloud.com>
2025-07-23 07:07:17 +0100

    change comments

--- AdRasta2/Converters/BoolToBrushConverter.cs
+++ AdRasta2/Converters/BoolToBrushConverter.cs
@@ -4,10 +4,11 @@ using System;
 using System.Globalization;
 
 namespace AdRasta2.Converters;
+
 public class BoolToBrushConverter : IValueConverter
 {
     public IBrush TrueBrush { get; set; } = new SolidColorBrush(Colors.AntiqueWhite); // Selected
-    public IBrush FalseBrush { get; set; } = new SolidColorBrush(Color.Parse("#BEBEBE"));             // Default
+    public IBrush FalseBrush { get; set; } = new SolidColorBrush(Color.Parse("#BEBEBE")); // Default
 
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         => value is true ? TrueBrush : FalseBrush;

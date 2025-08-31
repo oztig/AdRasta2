1bc4cd4db3eaa6ef68ecbf24c3d8db8f0db3a4a0
oztig <nj_pearson+github@icloud.com>
2025-08-31 10:07:57 +0100

    Split out bottom panel

---
+++ AdRasta2/Views/BottomPanelView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AdRasta2.Views;
+
+public partial class BottomPanelView : UserControl
+{
+    public BottomPanelView()
+    {
+        InitializeComponent();
+    }
+}

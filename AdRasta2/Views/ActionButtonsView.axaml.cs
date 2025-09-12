15f1804f4cb625ca8b1d6a06f83962a6b16d8598
oztig <nj_pearson+github@icloud.com>
2025-09-12 09:10:24 +0100

    split out remaining sections of screen into their own controls

---
+++ AdRasta2/Views/ActionButtonsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AdRasta2.Views;
+
+public partial class ActionButtonsView : UserControl
+{
+    public ActionButtonsView()
+    {
+        InitializeComponent();
+    }
+}
